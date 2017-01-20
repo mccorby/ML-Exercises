f732ac45ec7ac456d307d2f110e513f1b98bbbb3
Jose Corbacho <JoseAntonio.Corbacho@sky.uk>
2017-01-20 15:11:33 +0000

    Predict using NN forward propagation

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -7,12 +7,15 @@ function p = predict(Theta1, Theta2, X)
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 p = zeros(size(X, 1), 1);
+
+% Add ones to the X data matrix
+X = [ones(m, 1) X];
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
+%               your learned neural network. You should set p to a
 %               vector containing labels between 1 to num_labels.
 %
 % Hint: The max function might come in useful. In particular, the max
@@ -21,13 +24,11 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+a2 = sigmoid(X * Theta1');
+a2 = [ones(m, 1) a2];
+h = sigmoid(a2 * Theta2')
 
-
-
-
-
-
-
+[tmp, p] = max(h, [], 2);
 
 % =========================================================================
 
